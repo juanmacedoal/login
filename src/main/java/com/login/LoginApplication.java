0143c26ef171240ee83b9881d51d18745a84f768
MACEDO ALMEIDA Juan Manuel <juan-manuel.macedo@soprasteria.com>
2019-09-11 13:42:42 +0200

    init commit

---
+++ src/main/java/com/login/LoginApplication.java
@@ -0,0 +1,18 @@
+package com.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author jmmacedo
+ *
+ */
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
